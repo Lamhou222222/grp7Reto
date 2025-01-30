9690ed757c8128b19240003ef4a16e9c74c9dce9
tuerjfg@jhrfbgj.con <lamhour@gmail.com>
2025-01-30 08:24:07 +0100

    comitcomit

--- src/Reto/MostrarLocal.java
+++ src/Reto/MostrarLocal.java
@@ -20,4 +20,32 @@ public class MostrarLocal {
 	        }
 	    }
 
+	public static void mostrarOficinas(String dni) {
+	        System.out.println("\nSeleccione una oficina:");
+	        System.out.println("1. Oficina Donostia");
+	        System.out.println("2. Oficina Irún");
+	        System.out.println("3. Salir");
+	
+	        Scanner scanner = new Scanner(System.in);
+	        int opcion = scanner.nextInt();
+	        scanner.nextLine();
+	
+	        switch (opcion) {
+	            case 1:
+	                System.out.println("Has seleccionado la Oficina Donostia");
+	                Articulo.mostrarArticulos(dni);
+	                break;
+	            case 2:
+	                System.out.println("Has seleccionado la Oficina Irún");
+	                Articulo.mostrarArticulos(dni);
+	                break;
+	            case 3:
+	                System.out.println("Volver al menú principal.");
+	                break;
+	            default:
+	                System.out.println("Opción no válida. Volviendo al menú principal.");
+	        }
+	    }
+	
+
 }
